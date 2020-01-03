8c836c0ab307bb8b9352dc56d520d69d853a162a
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-02 21:36:37 -0500

    Reverse String Direct and testing added - Day 1

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace LeetCode
 {
     class Program
     {
-        static void Main(string[] s)
+        static void Main()
         {
+            int sum = 0;
+            const int _max = 10000000;
             Console.WriteLine(StringHelper.ReverseString("Hello World"));
+            Console.WriteLine(StringHelper.ReverseStringDirect("Hello World"));
+
+
+            // Version 1: reverse with TocharArray.
+            var s1 = Stopwatch.StartNew();
+            for (int i = 0; i < _max; i++)
+            {
+                sum += StringHelper.ReverseString("Hello World").Length;
+            }
+            s1.Stop();
+
+            // Version 2: reverse with iteration.
+            var s2 = Stopwatch.StartNew();
+            for (int i = 0; i < _max; i++)
+            {
+                sum += StringHelper.ReverseStringDirect("Hello World").Length;
+            }
+            s2.Stop();
+
+            Console.WriteLine(s1.Elapsed.TotalMilliseconds);
+            Console.WriteLine(s2.Elapsed.TotalMilliseconds);
+            
+            // somehow Reverse String Direct took longer??
         }
     }
 }
--- LeetCode/LeetCode/StringHelper.cs
+++ LeetCode/LeetCode/StringHelper.cs
@@ -12,5 +12,18 @@ namespace LeetCode
             Array.Reverse(arr);
             return new string(arr);
         }
+
+        public static string ReverseStringDirect(string s)
+        {
+            char[] array = new char[s.Length];
+            int forward = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                array[forward++] = s[i];
+            }
+            return new string(array);
+        }
+
+
     }
 }
