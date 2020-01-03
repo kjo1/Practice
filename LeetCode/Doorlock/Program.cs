0a34b4da08ad265becf1b80d9130d84e4966fe3e
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-03 17:33:17 -0500

    Basic door lock - Day 2

---
+++ LeetCode/Doorlock/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Doorlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] passcodeNumbers = { 2, 0, 2, 0 };
+            int[] userInput = new int[4];
+
+            while (true)
+            {
+                Console.WriteLine("Enter the first number");
+                userInput[0] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number");
+                userInput[1] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the third number");
+                userInput[2] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the fourth number");
+                userInput[3] = int.Parse(Console.ReadLine());
+
+                if (userInput[0] == passcodeNumbers[0]
+                    && userInput[1] == passcodeNumbers[1]
+                    && userInput[2] == passcodeNumbers[2]
+                    && userInput[3] == passcodeNumbers[3])
+                {
+                    Console.WriteLine("\nDoor is unlocked");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nThe numbers are incorrect\n Please try again\n");
+                }
+            }
+        }
+    }
+}
