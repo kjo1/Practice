6f2b19928d28b5dfb10821bf59b6b154607e390a
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-02 20:50:44 -0500

    Reverse String - Day 1

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] s)
+        {
+            Console.WriteLine(StringHelper.ReverseString("Hello World"));
+        }
+    }
+}
---
+++ LeetCode/LeetCode/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    static class StringHelper
+    {
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
