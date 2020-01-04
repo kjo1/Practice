e444ed29d75211efe4d022bc37b9c8452855a28b
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-04 13:45:38 -0500

    Basic method review - Day 3

---
+++ LeetCode/Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("You", 3);
+            SayHi("Me", 5);
+            SayHi("John", 30);
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
