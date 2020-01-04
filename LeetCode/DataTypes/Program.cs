7fc5fcda1327cd1d3a530a8a66ec0caf4d03ac12
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-04 13:00:21 -0500

    Working with numbers - Day 3

--- LeetCode/DataTypes/Program.cs
+++ LeetCode/DataTypes/Program.cs
@@ -23,6 +23,26 @@ namespace DataTypes
             Console.WriteLine(phrase[0]);
             Console.WriteLine(phrase.IndexOf("W"));
             Console.WriteLine(phrase.Substring(6, 2));
+
+            Console.WriteLine(4 + 2 * 3); // 10
+            Console.WriteLine((4 + 2) * 3); // 18
+            Console.WriteLine(5 / 2); // 2 int int int back
+            Console.WriteLine(5 / 2.0); // 2.5 int decimal decimal back
+
+            int num = 6;
+            num++;
+            Console.WriteLine(num);
+            num--;
+            Console.WriteLine(num);
+
+            Console.WriteLine(Math.Abs(-40)); // 40
+            Console.WriteLine(Math.Pow(3.8, 2)); // 14.44
+            Console.WriteLine(Math.Sqrt(36)); // 6
+            Console.WriteLine(Math.Max(4, 30)); // 30
+            Console.WriteLine(Math.Min(4, 30)); // 4
+            Console.WriteLine(Math.Round(3.2)); // 3
+            Console.WriteLine(Math.Round(3.6)); // 4
+
         }
     }
 }
