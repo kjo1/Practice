8816e3e4aaf189e25f5683e1bbb060c34022a073
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-04 13:19:32 -0500

    Basic caculator - Day 3

---
+++ LeetCode/Caculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
