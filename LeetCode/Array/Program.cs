e14aded271885205c0e181cf30aa3b916d942dc3
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-04 13:36:32 -0500

    Basic array review - Day 3

---
+++ LeetCode/Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 20, 18, 5, 22, 2, 45 };
+            string[] colors = new string[5];
+            colors[0] = "Green";
+            colors[1] = "Blue";
+
+            Console.WriteLine(colors[1]);
+
+            luckyNumbers[1] = 33;
+
+            Console.WriteLine(luckyNumbers[1]);
+        }
+    }
+}
