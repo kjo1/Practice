1702de279fcc0e8d1681576b8652f86c6e239557
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-03 20:49:28 -0500

    Review very basic variable usages - Day 2

---
+++ LeetCode/Giraffe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "You";
+            int characterAge;
+            characterAge = 20;
+
+            Console.WriteLine("Hello " + characterName + "!");
+            Console.WriteLine("You are " + characterAge + " years old!");
+
+            characterName = "me";
+            Console.WriteLine("You really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
