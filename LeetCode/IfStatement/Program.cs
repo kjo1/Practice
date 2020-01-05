291f5e59554ecd022cd3cea5b28d7c792e26b1cb
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-05 11:32:01 -0500

    If statement - Day 4

---
+++ LeetCode/IfStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            bool isTall = true;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a bamle but you are tall");
+            }
+            else if (isMale || isTall)
+            {
+                Console.WriteLine("You are either tall or a male");
+            }
+            else
+            {
+                Console.WriteLine("You are not male or not tall or both");
+            }
+        }
+    }
+}
---
+++ LeetCode/IfStatement2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfStatement2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 1, 11));
+        }
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+
+            return result;
+        }
+    }
+}
