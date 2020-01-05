fffbe5f4bbf16381726fe7e2d389c73baf0e979f
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-05 12:04:11 -0500

    Guessing Game - Day 4

---
+++ LeetCode/GuessingGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "practice";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 5;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You lose...");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+        }
+    }
+}
