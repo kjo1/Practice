6736c1fe172b1d459e1627ba92d79634dbab0639
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-05 11:52:42 -0500

    While Loop - Day 4

---
+++ LeetCode/WhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+        }
+    }
+}
