d0eb0d6ad2225739c366a0b1ad8f01647fd0282e
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-06 18:32:52 -0500

    For Loop - Day 5

---
+++ LeetCode/ForLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 5, 6, 2, 4 };
+            
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+        }
+    }
+}
