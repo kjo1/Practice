d04893fb8416e5919fec53e261b1e9af433b551b
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-07 23:18:19 -0500

    Class and Object - Day 6

---
+++ LeetCode/ClassAndObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Challenge";
+            book1.author = "Kanghyun Jo";
+            book1.pages = 333;
+
+            Book book2 = new Book();
+            book2.title = "Happiness";
+            book2.author = "Kanghyun Jo";
+            book2.pages = 777;
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.author);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
