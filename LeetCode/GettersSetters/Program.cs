b3fe38a602bc90fdf69db3a4e819202cfce1404d
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-08 16:22:05 -0500

    Setters & Getters - Day 7

---
+++ LeetCode/GettersSetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GettersSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            // G, PG, PG-13, R, NR
+            shrek.Rating = "RRR";
+            Console.WriteLine(avengers.Rating);
+            Console.WriteLine(shrek.Rating);
+        }
+    }
+}
