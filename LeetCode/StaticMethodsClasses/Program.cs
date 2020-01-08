a6ddbb0d1fa293d5c0b0d48d3ec11eadad6638f2
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-08 16:44:54 -0500

    Static Methods Classes - Day 7

---
+++ LeetCode/StaticMethodsClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticMethodsClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Math.Sqrt(144));
+            UsefulTools.SayHi("John");
+        }
+    }
+}
---
+++ LeetCode/StaticMethodsClasses/UsefulTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMethodsClasses
+{
+    static class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hi " + name);
+        }
+    }
+}
