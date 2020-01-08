5f20103362288d4f6558a6c58ae583525fe1d354
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-08 16:35:18 -0500

    Static Class Attributes - Day 7

---
+++ LeetCode/StaticClassAttributes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticClassAttributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            //Console.WriteLine(Song.songCount);
+
+            Console.WriteLine(kashmir.getSongCount());
+
+            Console.WriteLine(holiday.title);
+            Console.WriteLine(kashmir.title);
+
+        }
+    }
+}
