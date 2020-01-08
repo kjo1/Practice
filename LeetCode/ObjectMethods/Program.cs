c678e73d38700f6dae5d8914b4db1910d54595b2
kjo1 <kjo1@ncstudents.niagaracollege.ca>
2020-01-08 16:02:17 -0500

    Object Methods - Day 7

---
+++ LeetCode/ObjectMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ObjectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("John", "Computer Science", 3.6);
+            Student student2 = new Student("Jane", "Business", 2.3);
+
+            Console.WriteLine(student1.HasHonours());
+            Console.WriteLine(student2.HasHonours());
+        }
+    }
+}
---
+++ LeetCode/ObjectMethods/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMethods
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string Name, string Major, double Gpa)
+        {
+            name = Name;
+            major = Major;
+            gpa = Gpa;
+        }
+
+        public bool HasHonours()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
